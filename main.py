9fdaafd5260074e5ee855d0d4ac7e1ed179511d7
Johnnpham7496 <johnpham7496@gmail.com>
2023-06-26 20:47:12 -0500

    added main.py file with hello world endpoint

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def hello_world():
+    return {"message": "Hello World"}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
