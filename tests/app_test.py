b9342767d13eedad5dc0d1212599d8c8fc0f5c09
Johnnpham7496 <johnpham7496@gmail.com>
2023-06-27 19:20:57 -0500

    added health endpoint test

--- tests/app_test.py
+++ tests/app_test.py
@@ -13,3 +13,12 @@ def test_tc_0001_welcome():
 
     assert response.status_code == 200
     assert response.json() == td_message
+
+
+def test_tc0002_health():
+    td_messgae = {'status': 'OK'}
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json() == td_messgae
