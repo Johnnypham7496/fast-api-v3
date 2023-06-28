a4915b233072ed12415da8947001b302301cf9eb
Johnnpham7496 <johnpham7496@gmail.com>
2023-06-28 17:38:01 -0500

    create router file and refactor endoints to use router

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from fastapi import FastAPI, Response, status
-from db import users_db
-from sqlalchemy.orm import Session
-from fastapi import FastAPI, Response, status, Depends
-from db_config import engine, get_db
-from repository import users_repository
+from fastapi import FastAPI
+from router import app_router
 import uvicorn
 
 
@@ -15,26 +11,17 @@ app = FastAPI(
 )
 
 
-@app.get("/", tags=['Welcome'], response_description='Displays welcome message')
-async def welcome(response: Response):
-    response.status_code=status.HTTP_200_OK
-    return {"message": "Hello, welcome to the Justice League's FastAPI"}
+app.include_router(app_router.router)
 
 
-@app.get("/health", tags= ['Health'], response_description='Returns the health status of this endpoint')
-async def health(response: Response):
-    response.status_code=status.HTTP_200_OK
-    return {"status": "OK"}
-
-
-@app.get("/dbsetup")
-async def db_setup(db: Session = Depends(get_db)):
-    users_db.Base.metadata.drop_all(engine)
-    users_db.Base.metadata.create_all(engine)
-    users_repository.add_user_td(db)
-    response_text = '{"message": "Database Created"}'
-    response = Response(content=response_text, status_code=status.HTTP_200_OK, media_type='application/json')
-    return response
+# @app.get("/dbsetup")
+# async def db_setup(db: Session = Depends(get_db)):
+#     users_db.Base.metadata.drop_all(engine)
+#     users_db.Base.metadata.create_all(engine)
+#     users_repository.add_user_td(db)
+#     response_text = '{"message": "Database Created"}'
+#     response = Response(content=response_text, status_code=status.HTTP_200_OK, media_type='application/json')
+#     return response
 
 
 if __name__ == "__main__":
