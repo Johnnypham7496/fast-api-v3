c9412cdb9c3ce0b70fa2381ec70ac462ce354072
Johnnpham7496 <johnpham7496@gmail.com>
2023-07-07 12:28:40 -0500

    separated jobs functions from users_repository to jobs_repository

--- router/jobs_router.py
+++ router/jobs_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Response, status, Depends
 from sqlalchemy.orm import Session
 from schemas import JobsModel
-from repository import users_repository
+from repository import jobs_repository
 from db_config import get_db
 from typing import List
 
@@ -14,6 +14,6 @@ router = APIRouter(
 
 @router.get('/', response_description='Display all jobs', description='Retrieves all jobs', response_model=List[JobsModel])
 def get_all_jobs(response: Response, db: Session= Depends(get_db)):
-    return_value = users_repository.get_all_jobs(db)
+    return_value = jobs_repository.get_all_jobs(db)
     response.status_code=status.HTTP_200_OK
     return return_value
