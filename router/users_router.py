ff882e6983e3b38db6b156100fa748d4a6408272
Johnnpham7496 <johnpham7496@gmail.com>
2023-07-16 21:18:29 -0500

    added post function to create users

--- router/users_router.py
+++ router/users_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Response, status, Depends, HTTPException
 from sqlalchemy.orm import Session
-from schemas import UserModel, MessageModel
+from schemas import UserModel, MessageModel, CreateUserModel
 from repository import users_repository
 from db_config import get_db
 from typing import List
@@ -30,3 +30,37 @@ def get_by_username(response: Response, username: str, db: Session= Depends(get_
     response.headers['message'] = 'user found'
     response.status_code = status.HTTP_200_OK
     return return_value
+
+
+@router.post('/', response_description='Creates a new user', description='Creates a new user', response_model=UserModel, status_code=status.HTTP_201_CREATED, responses= {400: {"model": MessageModel}, 409: {"model": MessageModel}})
+def create_user(request: CreateUserModel, response: Response, db: Session= Depends(get_db)):
+    username_request = request.username
+    email_request = request.email
+    role_request = request.role
+
+    existing_username = users_repository.get_by_username(db, username_request)
+    existing_email = users_repository.get_by_email(db, email_request)
+
+    if existing_username!= None:
+        response_text = 'username already exists. Please check your parameter and try again.'
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=response_text)
+    
+    if existing_email!= None:
+        response_text = 'email already exists. Please check your parameter and try again.'
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=response_text)
+    
+    if username_request.strip() == '':
+        response_text = 'username cannot be empty. Please check your parameter and try again.'
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=response_text)
+    
+    if email_request.strip() == '':
+        response_text = 'email cannot be empty. Please check your parameter and try again.'
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=response_text)
+    
+    if role_request.strip() == '':
+        response_text = 'role cannot be empty. Please check your parameter and try again.'
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=response_text)
+    
+    response.status_code = status.HTTP_201_CREATED
+    response.headers['Location'] = '/users/v1/' + str(username_request.strip())
+    return users_repository.add_user(db, username_request, email_request, role_request)
