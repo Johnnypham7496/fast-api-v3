6f29f26b575a8a4cea8b8b75c9e101f34e89d681
Johnnpham7496 <johnpham7496@gmail.com>
2023-08-10 21:16:05 -0500

    completed put function to update user

--- router/users_router.py
+++ router/users_router.py
@@ -66,11 +66,72 @@ def create_user(request: CreateUserModel, response: Response, db: Session= Depen
     return users_repository.add_user(db, username_request, email_request, role_request)
 
 
-@router.put('/{username}', response_description='Successfully updated user info', description='Updating user record', status_code=status.HTTP_204_NO_CONTENT, responses={204: {'model': None}, status.HTTP_400_BAD_REQUEST: {'model': MessageModel}, status.HTTP_404_NOT_FOUND: {'model', MessageModel}})
-def update_user(response: Response, _username: str, request: UpdateUserModel, db: Session = Depends(get_db)):
+@router.put('/{username}', response_description='Successfully updated user info', description='Updating user record', status_code=status.HTTP_204_NO_CONTENT, responses={204: {"model": None}, status.HTTP_400_BAD_REQUEST: {"model": MessageModel}, status.HTTP_404_NOT_FOUND: {"model": MessageModel}})
+def update_user(_username: str, request: UpdateUserModel, response: Response, db: Session = Depends(get_db)):
     email_request = request.email
     role_request = request.role
 
     if not email_request and not role_request: 
         response_text = 'response body cannot be empty. Please check your parameter and try again.'
-        raise HTTPException(status_code=status.400, detail=response_text)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= response_text)
+    
+
+    username_check = users_repository.get_by_username(db, _username)
+
+    if not username_check:
+        response_text = 'username does not exist. Please check your parameter and try again.'
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= response_text)
+    
+    if email_request:
+        email_request = email_request.strip()
+    else:
+        email_request = ''
+
+    if role_request:
+        role_request = role_request.strip()
+    else:
+        role_request = ''
+
+
+    if email_request == '' and role_request == '':
+        response_text = 'response body fields cannot be empty. Please check your payload and try again.'
+        raise HTTPException(status_code=400, detail=response_text)
+    
+    response.status_code = status.HTTP_204_NO_CONTENT
+    return users_repository.update_user(db, _username, email_request, role_request)
+
+
+
+# @router.put('/{username}', response_description='Successfully updated user info', description='Updating user record', status_code=status.HTTP_204_NO_CONTENT, responses= {status.HTTP_204_NO_CONTENT: {"model": None}, status.HTTP_400_BAD_REQUEST: {'model': MessageModel}, status.HTTP_404_NOT_FOUND: {'model': MessageModel}})
+# def update_user(username: str, request: UpdateUserModel, response: Response, db: Session = Depends(get_db)):
+#     email_request = request.email
+#     role_request = request.role
+
+
+#     if not email_request and not role_request:
+#         response_text = 'request body cannot be empty. Please check your payload and try again.'
+#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= response_text)
+
+#     user_check = user_repository.get_by_username(db, username)
+
+#     if not user_check:
+#         response_text = 'username already exists. Please check your parameter and try again.'
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=response_text)
+
+#     if email_request: 
+#         email_request = email_request.strip()
+#     else:
+#         email_request = ''
+
+#     if role_request:
+#         role_request = role_request.strip()
+#     else:
+#         email_request = ''
+
+#     if email_request == '' and role_request == '':
+#         response_text = 'request body fields cannot be empty. Please check your payload and try again.'
+#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=response_text)
+
+
+#     response.status_code = status.HTTP_204_NO_CONTENT
+#     return user_repository.update_user(db, username, email_request, role_request)
