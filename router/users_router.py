2cd8b2b5f178484bf3f30dbf2b269571fc682909
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-03 15:49:56 -0500

    changed syntax to using double quotes instead of one because of testcase failure

--- router/users_router.py
+++ router/users_router.py
@@ -66,35 +66,35 @@ def create_user(request: CreateUserModel, response: Response, db: Session= Depen
     return users_repository.add_user(db, username_request, email_request, role_request)
 
 
-@router.put('/{username}', response_description='Successfully updated user info', description='Updating user record', status_code=status.HTTP_204_NO_CONTENT, responses={204: {"model": None}, status.HTTP_400_BAD_REQUEST: {"model": MessageModel}, status.HTTP_404_NOT_FOUND: {"model": MessageModel}})
+@router.put("/{username}", response_description="Successfully updated user info", description="Updating a single user record", status_code=204, responses={204: {"model": None}, 400: {"model": MessageModel}, 404: {"model": MessageModel}})
 def update_user(_username: str, request: UpdateUserModel, response: Response, db: Session = Depends(get_db)):
     email_request = request.email
     role_request = request.role
 
     if email_request == None and role_request == None: 
-        response_text = 'response body cannot be empty. Please check your parameter and try again.'
+        response_text = "request body cannot be empty. Please check your parameter and try again."
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= response_text)
     
 
     username_check = users_repository.get_by_username(db, _username)
 
     if username_check == None:
-        response_text = 'username does not exist. Please check your parameter and try again.'
+        response_text = "username does not exist. Please check your parameter and try again."
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= response_text)
     
     if email_request != None:
         email_request = email_request.strip()
     else:
-        email_request = ''
+        email_request = ""
 
     if role_request != None:
         role_request = role_request.strip()
     else:
-        role_request = ''
+        role_request = ""
 
 
-    if email_request == '' and role_request == '':
-        response_text = 'response body fields cannot be empty. Please check your payload and try again.'
+    if email_request == "" and role_request == "":
+        response_text = "response body fields cannot be empty. Please check your payload and try again."
         raise HTTPException(status_code=400, detail=response_text)
     
     response.status_code = status.HTTP_204_NO_CONTENT
