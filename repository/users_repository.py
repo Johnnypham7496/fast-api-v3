47666af7936d6817c928451b7d5372f7631966d5
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-05 17:16:43 -0500

    added delete_user function to users_repository.py

--- repository/users_repository.py
+++ repository/users_repository.py
@@ -40,3 +40,8 @@ def update_user(db: Session, _username, _email, _role):
         user_to_update.role = _role
 
     db.commit()
+
+
+def delete_user(db: Session, _username):
+    db.query(UserDb).filter(UserDb.username == _username).delete()
+    db.commit()
