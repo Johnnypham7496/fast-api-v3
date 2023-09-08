69f2c00a68389e8db8ccd8d80b14116861586af9
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-07 22:08:58 -0500

    added handling exception error

--- outside_api_call.py
+++ outside_api_call.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import HTTPError
 
 response = requests.get('https://api.github.com')
 
@@ -6,3 +7,17 @@ if response.status_code == 200:
     print('Success!')
 else:
     print('An error occurred!')
+
+
+for url in ['https://api.github.com', 'https://api.github.com/invalid']:
+    try:
+        response = requests.get(url)
+
+        # If the response is successful, no Exceptions will be raised
+        response.raise_for_status()
+    except HTTPError as http_error:
+        print(f'HTTP error occurred: {http_error}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        print('Success')
